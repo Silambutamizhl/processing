cb3eb6f0eb63cb40222f46117cf13e125d4a629f
Silambarasan T <117810169+Silambutamizhl@users.noreply.github.com>
2022-11-30 17:27:13 +0530

    Add files via upload

---
+++ qualliz/index.js
@@ -0,0 +1,13 @@
+
+
+.icons i{
+    color: #fff;
+    padding: 8px;
+    font-size: 24px;
+    background-color: #2D83EE;
+    
+}
+<div class="icons">
+<i class="fa-sharp fa-solid fa-bars" onclick="openMenu()"></i>
+
+</div>
